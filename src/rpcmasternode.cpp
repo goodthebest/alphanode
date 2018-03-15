3ecf8c1d2b69b9fce4d6246a4665888f1b67f19c
alphanode <admin@cloud2b.de>
2018-03-15 01:28:42 +0100

    minor changes

--- src/rpcmasternode.cpp
+++ src/rpcmasternode.cpp
@@ -84,7 +84,7 @@ Value Darksend(const Array& params, bool fHelp)
 
     CBitcoinAddress address(params[0].get_str());
     if (!address.IsValid())
-        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid ALN address");
+        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Alphanode address");
 
     // Amount
     CAmount nAmount = AmountFromValue(params[1]);
